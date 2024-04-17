547fd6562ed66e344e5ff8cc918534d49406927e
Gilmarrr123 <124006474+Gilmarrr123@users.noreply.github.com>
2024-04-17 09:11:38 -0300

    Update web.py

--- aulinha/web.py
+++ aulinha/web.py
@@ -8,11 +8,12 @@ from datetime import datetime
 from time import sleep
 import os
 
-
+# Configuração do WebDriver para o firefox (certificar se o geckodriver está instalado)
 driver = webdriver.Firefox()
 
+# Abre o Site
 driver.get('https://imoveisfranca.com.br/resultadoBusca/alugar&tipo=alugar')
 
-
+# Clicar no botão de CRM
 clicar_crm = driver.find_element(By.XPATH, '//*[@id="resultado-busca"]/div/div/div[2]/div[2]/div/div[3]/div/div[2]/a/h5').text
 print(clicar_crm)
